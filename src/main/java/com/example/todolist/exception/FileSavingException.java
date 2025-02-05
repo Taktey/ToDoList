04af181265117c8bd7f9efe71d05692a01e7288b
taktey <79062502914@yandex,ru>
2025-02-05 14:09:29 +0700

    FileSavingException.java added to FileService.java o[load method

--- src/main/java/com/example/todolist/exception/FileSavingException.java
+++ src/main/java/com/example/todolist/exception/FileSavingException.java
@@ -1,15 +1,14 @@
 package com.example.todolist.exception;
 
-import java.io.IOException;
 
-public class FileSavingException extends IOException {
-    private static final String FAILED_TO_SAVE_FILE_MSG = "Failed to save file";
+public class FileSavingException extends RuntimeException {
+    private static final String FAILED_TO_SAVE_FILE_MSG = "Failed to save file!";
 
     public FileSavingException() {
         super(FAILED_TO_SAVE_FILE_MSG);
     }
 
     public FileSavingException(String message) {
-        super(message);
+        super(FAILED_TO_SAVE_FILE_MSG + "\n" + message);
     }
 }
